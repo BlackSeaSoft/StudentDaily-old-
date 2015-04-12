aafaf7ea1a62302d3740ffa9664c44611fad89ae
hameltomor <hameltomor@gmail.com>
2015-04-12 18:36:28 +0300

    add registration, login, logout, auth

---
+++ Server/database.php
@@ -0,0 +1,44 @@
+<?php
+/**
+  * Функция для подключения к СУБД MySQL.
+  * Функция не принимает никаких параметров.
+  * Функция предназначена для использования, в основном,
+  * с одной базой данных
+  */
+function db_connect() {
+		$host = 'mysql.hostinger.ru';
+		$user = 'u568080489_admin';
+		$password = 'qwerty123';
+		$bd = 'u568080489_mybd';
+		
+		
+		$connection = mysqli_connect($host, $user, $password);
+		
+		if(!$connection || !$connection->select_db($bd)) throw new Exception('Ошибка подключения к MySQL');
+		
+		return $connection;
+}
+
+function db_result_to_array($result, $bool_fetch=0){
+		$res_array = array();
+		
+		$count = 0;
+		if($bool_fetch == 0)
+		{
+			while($row = $result->fetch_array(MYSQLI_ASSOC))
+			{
+				$res_array[$count] = $row;
+				$count++;
+			}
+		}
+		else
+		{
+			while($row = $result->fetch_array(MYSQLI_NUM))
+			{
+				$res_array[$count] = $row;
+				$count++;
+			}
+		}
+		return $res_array;
+}
+?>
